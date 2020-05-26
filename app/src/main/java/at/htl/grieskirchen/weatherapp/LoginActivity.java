45236c76158101f4ca1ecaa91ca45314177bcfea
Skoerven <nigr2002511>
2020-05-26 14:01:23 +0200

    -Login logik funkt

--- app/src/main/java/at/htl/grieskirchen/weatherapp/LoginActivity.java
+++ app/src/main/java/at/htl/grieskirchen/weatherapp/LoginActivity.java
@@ -102,6 +102,8 @@ public class LoginActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this, "Authentication worked.",
+                                    Toast.LENGTH_SHORT).show();
                             updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
