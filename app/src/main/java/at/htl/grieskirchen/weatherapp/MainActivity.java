21e84d124d766609e677396e9579d935ea7a4e57
BenjaminHeissenberger <heissenbergerbenjamin@gmail.com>
2020-06-25 13:14:24 +0200

    WeatherTask independent

--- app/src/main/java/at/htl/grieskirchen/weatherapp/MainActivity.java
+++ app/src/main/java/at/htl/grieskirchen/weatherapp/MainActivity.java
@@ -150,126 +150,7 @@ private ImageButton btn_settings;
     public static MainActivity getInstance() {
         return sInstance;
     }
-    class WeatherTask extends AsyncTask<String, Void, String> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
 
-            /* Showing the ProgressBar, Making the main design GONE */
-            //findViewById(R.id.loader).setVisibility(View.VISIBLE);
-            //findViewById(R.id.mainContainer).setVisibility(View.GONE);
-            //findViewById(R.id.errorText).setVisibility(View.GONE);
-        }
-
-        protected String doInBackground(String... args) {
-            String response = HttpRequest.excuteGet(
-                    "https://api.openweathermap.org/data/2.5/weather?lat=" + args[0] +  "&lon=" + args[1] + "&units=metric&appid=" + API);
-            return response;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-
-
-            try {
-                JSONObject jsonObj = new JSONObject(result);
-                JSONObject main = jsonObj.getJSONObject("main");
-                JSONObject sys = jsonObj.getJSONObject("sys");
-                JSONObject wind = jsonObj.getJSONObject("wind");
-                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
-
-                Long updatedAt = jsonObj.getLong("dt");
-                String updatedAtText = "Updated at: " + new SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.GERMANY).format(new Date(updatedAt * 1000));
-                String temp = Math.round(Double.parseDouble(main.getString("temp"))) + "°C";
-                String tempMin = "Min Temp: " + Math.round(Double.parseDouble(main.getString("temp_min"))) + "°C";
-                String tempMax = "Max Temp: " + Math.round(Double.parseDouble(main.getString("temp_max"))) + "°C";
-                String pressure = main.getString("pressure")+ " hPa";
-                String humidity = main.getString("humidity")+ " %";
-
-                Long sunrise = sys.getLong("sunrise");
-                Long sunset = sys.getLong("sunset");
-                String windSpeed = wind.getString("speed")+ " m/s";
-                String weatherDescription = weather.getString("description");
-
-                String address = CITY;
-//                addressTxt = findViewById(R.id.address);
-//                addressTxt.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//
-//                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.getInstance());
-//                            builder.setTitle("Enter Text");
-//
-//// Set up the input
-//                            final EditText input = new EditText(MainActivity.getInstance());
-//// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-//                            input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
-//                            builder.setView(input);
-//
-//// Set up the buttons
-//                            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//                                @Override
-//                                public void onClick(DialogInterface dialog, int which) {
-//                                    CITY = input.getText().toString();
-//                                    write(CITY);
-//                                    finish();
-//                                    startActivity(getIntent());
-//
-//                                }
-//                            });
-//                            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-//                                @Override
-//                                public void onClick(DialogInterface dialog, int which) {
-//                                    dialog.cancel();
-//                                }
-//                            });
-//
-//                            builder.show();
-//
-//
-//                    }
-//                });
-//                updated_atTxt = findViewById(R.id.updated_at);
-//                statusTxt = findViewById(R.id.status);
-//                tempTxt = findViewById(R.id.temp);
-//                temp_minTxt = findViewById(R.id.temp_min);
-//                temp_maxTxt = findViewById(R.id.temp_max);
-//                sunriseTxt = findViewById(R.id.sunrise);
-//                sunsetTxt = findViewById(R.id.sunset);
-//                windTxt = findViewById(R.id.wind);
-//                pressureTxt = findViewById(R.id.pressure);
-//                humidityTxt = findViewById(R.id.humidity);
-//
-//                addressTxt.setText(address);
-//                updated_atTxt.setText(updatedAtText);
-//                statusTxt.setText(weatherDescription.toUpperCase());
-//                tempTxt.setText(temp);
-//                temp_minTxt.setText(list.get(position).getTempMin());
-//                temp_maxTxt.setText(list.get(position).getTempMax());
-//                sunriseTxt.setText(new SimpleDateFormat("hh:mm").format(new Date(list.get(position).getSunrise() * 1000)));
-//                sunsetTxt.setText(new SimpleDateFormat("hh:mm").format(new Date(list.get(position).getSunset() * 1000)));
-//                windTxt.setText(list.get(position).getWindSpeed());
-//                pressureTxt.setText(list.get(position).getPressure());
-//                humidityTxt.setText(list.get(position).getHumidity());
-               weatherList.add(new Weather(address, updatedAtText, temp, tempMin, tempMax, pressure, humidity, sunrise, sunset,windSpeed, weatherDescription));
-
-                sliderAdapter.notifyDataSetChanged();
-
-
-//                                    startActivity(getIntent());
-
-                /* Views populated, Hiding the loader, Showing the main design */
-                //findViewById(R.id.loader).setVisibility(View.GONE);
-                //findViewById(R.id.mainContainer).setVisibility(View.VISIBLE);
-
-
-            } catch (JSONException e) {
-                //findViewById(R.id.loader).setVisibility(View.GONE);
-                //findViewById(R.id.errorText).setVisibility(View.VISIBLE);
-            }
-
-        }
-    }
 
     private void aktuelisierdieduhuan() {
 
@@ -377,7 +258,7 @@ private ImageButton btn_settings;
                     String[] latlon = address.split(";");
                     newlat = Double.valueOf(latlon[0]);
                     newlot = Double.valueOf(latlon[1]);
-                    weatherTask = new WeatherTask();
+                    weatherTask = new WeatherTask(CITY,sliderAdapter,weatherList);
                     weatherTask.execute(String.valueOf(newlat),String.valueOf(newlot));
                     Log.d(TAG, "handleMessage: Lat" + newlat + " lot " + newlot  );
 
